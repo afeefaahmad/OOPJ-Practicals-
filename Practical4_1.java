a58176f7378d62ad0dd2b1c927808719010169fe
Afeefa Ahmad <75202156+afeefaahmad@users.noreply.github.com>
2022-04-04 08:14:20 +0100

    Create Practical4_1.java

---
+++ Practical4_1.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+public class Main
+{
+	public static void main(String[] args) {
+	    Random r = new Random(1000);
+	    for(int i=0; i<100; i++)
+		System.out.print("|" + r.nextInt(49) + "|");
+	}
+}
